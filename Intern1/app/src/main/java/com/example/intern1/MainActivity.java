00161714c93d01ddb13a7611b0c451aa770508b3
DNA <ducnguyen6693@gmail.com>
2024-09-12 16:38:00 +0700

    commit

---
+++ Intern1/app/src/main/java/com/example/intern1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.intern1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, child_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Intern1/app/src/main/java/com/example/intern1/button.java
@@ -0,0 +1,8 @@
+package com.example.intern1;
+
+import android.view.View;
+
+public class button {
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+    }
+}
