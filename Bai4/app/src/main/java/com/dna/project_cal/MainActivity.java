6fcf7c9a6aaddd89e66f63ef74f8f1406c56c36e
dna <ducnguyen6693@gmail.com>
2024-10-09 13:59:08 +0700

    done

--- Bai4/app/src/main/java/com/dna/project_cal/MainActivity.java
+++ Bai4/app/src/main/java/com/dna/project_cal/MainActivity.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 import java.text.DecimalFormat;
 
 public class MainActivity extends Activity {
     EditText edtdoC, edtdoF;
-    Button btncf,btnfc;
+    Button btncf, btnfc, btnClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,27 +18,38 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         edtdoC = findViewById(R.id.edtdoC);
         edtdoF = findViewById(R.id.edtdoF);
-        btncf = findViewById(R.id.btnFar);
-        btnfc = findViewById(R.id.btnCel);
-        btncf.setOnClickListener(new View.OnClickListener() {
+        btnfc = findViewById(R.id.btnFar);
+        btncf = findViewById(R.id.btnCel);
+        btnClear = findViewById(R.id.btnClear);
 
+        btncf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                DecimalFormat dcf=new DecimalFormat("#.00");
-                String doC = edtdoC.getText()+"";
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doC = edtdoC.getText() + "";
 
-                int C=Integer.parseInt(doC);
-                edtdoF.setText(""+dcf.format(C*1.8+32));
+                int C = Integer.parseInt(doC);
+                edtdoF.setText(dcf.format(C * 1.8 + 32));
             }
         });
 
         btnfc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DecimalFormat dcf=new DecimalFormat("#.00");
-                String doF = edtdoF.getText()+""; int F=Integer.parseInt(doF);
-                edtdoC.setText(""+dcf.format((F-32)/1.8));
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doF = edtdoF.getText() + "";
+                int F = Integer.parseInt(doF);
+                edtdoC.setText(dcf.format((F - 32) / 1.8));
             }
         });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtdoC.setText("");
+                edtdoF.setText("");
+            }
+        });
+    }
 }
