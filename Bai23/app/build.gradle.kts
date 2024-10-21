006f69a280099687129a2fe92bf2ba287d3b5130
DNA <ducnguyen6693@gmail.com>
2024-10-21 09:22:59 +0700

    ex23

--- Bai23/app/build.gradle.kts
+++ Bai23/app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
 
+    implementation("org.jbundle.util.osgi.wrapped:org.jbundle.util.osgi.wrapped.org.apache.http.client:4.1.2")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
